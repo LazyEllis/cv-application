9493c3d9e8d3c2680e884529d6b601fe71c6137a
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2024-10-05 00:24:25 +0100

    Reorganize CV layout for better clarity
    - Adjusted CVSection to display time ranges more consistently between
      Education and Experience sections
      - Moved time range display logic to a reusable `timeRange` variable
        for cleaner code
    - Updated field positions to improve layout structure:
      - Education: School name and location displayed first, followed by
        degree and time range
      - Experience: Position and time range displayed first, followed by
        company and location

--- src/components/cv-section.jsx
+++ src/components/cv-section.jsx
@@ -1,31 +1,43 @@
-const CVSection = (props) =>
-  !Object.values(props.details).every((detail) => detail === "") && (
-    <section>
-      <h3>{props.title}</h3>
-      <div className="cv-section-details">
-        <div className="cv-section-detail">
-          <div>
-            <h4>
-              {props.title === "Education"
-                ? props.details.school
-                : props.details.company}
-            </h4>
-            <div>{props.details.location}</div>
-          </div>
-          <div>
+const CVSection = (props) => {
+  const timeRange = `${props.details.startMonth.slice(0, 3)} ${
+    props.details.startYear
+  } - ${props.details.endMonth.slice(0, 3)} ${props.details.endYear}`;
+
+  return (
+    !Object.values(props.details).every((detail) => detail === "") && (
+      <section>
+        <h3>{props.title}</h3>
+        <div className="cv-section-details">
+          <div className="cv-section-detail">
             <div>
-              {props.title === "Education"
-                ? props.details.degree
-                : props.details.position}
+              <h4>
+                {props.title === "Education"
+                  ? props.details.school
+                  : props.details.position}
+              </h4>
+              <div>
+                {props.title === "Education"
+                  ? props.details.location
+                  : timeRange}
+              </div>
             </div>
             <div>
-              {props.details.startMonth.slice(0, 3)} {props.details.startYear} -{" "}
-              {props.details.endMonth.slice(0, 3)} {props.details.endYear}
+              <div>
+                {props.title === "Education"
+                  ? props.details.degree
+                  : props.details.company}
+              </div>
+              <div>
+                {props.title === "Education"
+                  ? timeRange
+                  : props.details.location}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    )
   );
+};
 
 export default CVSection;
